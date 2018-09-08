9baf2113f249623b7850f2329a339fcbd2d19121
Kamil Szestowicki <kamil.szestowicki@gmail.com>
2018-09-08 09:54:12 +0200

    car class - firs draft

---
+++ src/training/oop/manufacter/Car.java
@@ -0,0 +1,21 @@
+package training.oop.manufacter;
+
+class Car {
+    private String colour;
+    private String brand;
+    private String model;
+    private int yearOfProd;
+    private String sortOf;
+    private int availableSeats;
+
+    void start(){};
+    void stop(){};
+    void runForward(){};
+    void runBack(){};
+    void turn(String direction){};
+    void speedUp(){};
+    void speedDown(){};
+
+    public Car() {
+    }
+}
