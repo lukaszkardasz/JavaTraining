b43238aba25d7cc5cc711b57ec54d862b884cee8
[n2god] <[n2god@o2.pl]>
2019-04-22 22:34:18 +0200

    Tokenizacja - exercise finally ended!

--- src/main/java/training/Files/Tokenizacja/Main.java
+++ src/main/java/training/Files/Tokenizacja/Main.java
@@ -1,11 +1,41 @@
 package training.Files.Tokenizacja;
 
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class Main {
+
+    public static final String FILE_NAME = "plik.txt";
 
     public static void main(String[] args) {
 
+    Towar[] towar = new Towar[3];
+
+    towar[0] = new Towar();
+    towar[1] = new Towar(29.00, "Pieluchy");
+    towar[2] = new Towar(39.00, "Gruszki", 2019,11,28);
 
 
 
+        try {
+            PrintWriter writer = new PrintWriter(new FileWriter(FILE_NAME));
+            Towar.SaveToFile(towar, writer);
+            writer.close();
+
+            BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME));
+            Towar[] towarZpliku = Towar.writeFromFile(reader);
+
+            for (int i = 0; i < towarZpliku.length ; i++){
+
+                System.out.println(towarZpliku[i]);
+            }
+
+            reader.close();
+
+
+            } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
--- src/main/java/training/Files/Tokenizacja/Towar.java
+++ src/main/java/training/Files/Tokenizacja/Towar.java
@@ -1,29 +1,101 @@
 package training.Files.Tokenizacja;
 
-import java.time.LocalDateTime;
-import java.time.Month;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.StringTokenizer;
 
 public class Towar {
 
+    private static final String separator = ";";
+    private Date dataWydania;
+    private String nazwa;
+    private double cena;
+
     public Towar() {
         this.cena = 0.0;
-        this.nazwa = "";
-        this.dataWydania = LocalDateTime.now();
+        this.nazwa = " ";
+        this.dataWydania = new GregorianCalendar().getTime();
     }
 
-    public Towar(double cena, String nazwa){
+    public Towar(double cena, String nazwa) {
         this();
-        this.cena=cena;
-        this.nazwa=nazwa;
-    }
-
-    public Towar(double cena, String nazwa, int year, Month month, int dayOfMonth, int hour, int minute) {
         this.cena = cena;
         this.nazwa = nazwa;
+    }
+
+    public Towar(double cena, String nazwa, int year, int month, int dayOfMonth) {
+        this(cena, nazwa);
+        GregorianCalendar kalendarz = new GregorianCalendar(year, month - 1, dayOfMonth);
+        this.dataWydania = kalendarz.getTime();
+    }
+
+    public double getCena() {
+        return cena;
+    }
+
+    public void setCena(double cena) {
+        this.cena = cena;
+    }
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public Date getDataWydania() {
+        return dataWydania;
+    }
+
+    public void setDataWydania(Date dataWydania) {
+        this.dataWydania = dataWydania;
+    }
+
+    @Override
+    public String toString() {
+        GregorianCalendar kalendarz = new GregorianCalendar();
+        kalendarz.setTime(this.getDataWydania());
+        return String.format("Towar{cena= %s zł, nazwa= '%s', dataWydania: %d.%d.%d r.}", getCena(), getNazwa(), kalendarz.get(Calendar.DAY_OF_MONTH), kalendarz.get(Calendar.MONTH)+1, kalendarz.get(Calendar.YEAR));
+    }
+
+    public static void SaveToFile(Towar[] towar, PrintWriter outS) {
+        outS.println(towar.length);
+        GregorianCalendar kalendarz = new GregorianCalendar();
+
+        for (Towar t : towar) {
+            kalendarz.setTime(t.getDataWydania());
+            outS.println(t.getCena() +
+                    separator + t.getNazwa() +
+                    separator + kalendarz.get(Calendar.DAY_OF_MONTH) +
+                    separator + (kalendarz.get(Calendar.MONTH)+1) +
+                    separator + kalendarz.get(Calendar.YEAR));
+        }
 
     }
 
-    double cena;
-    String nazwa;
-    LocalDateTime dataWydania;
+    public static Towar[] writeFromFile(BufferedReader inS) throws IOException {
+        int dl = Integer.parseInt(inS.readLine());
+        Towar[] towar = new Towar[dl];
+        for (int i = 0; i < dl; i++) {
+            String linia = inS.readLine();
+            StringTokenizer tokeny = new StringTokenizer(linia, separator);
+            double cena = Double.parseDouble(tokeny.nextToken());
+            String nazwa = tokeny.nextToken();
+            int day = Integer.parseInt(tokeny.nextToken());
+            int month = Integer.parseInt(tokeny.nextToken());
+            int year = Integer.parseInt(tokeny.nextToken());
+            towar[i] = new Towar(cena, nazwa, year, month, day);
+        }
+
+        return towar;
+    }
+
 }
+
